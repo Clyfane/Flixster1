934a7ba3116c9a98b067c5823a1dd360ddc2e1c4
Clyfane <107255231+Clyfane@users.noreply.github.com>
2022-08-01 18:15:12 -0400

    Initial commit

---
+++ app/src/main/java/com/example/flixster1/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.flixster1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
+import models.Movie;
+import okhttp3.Headers;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    public static final String TAG = "MainActivity"; List<Movie> movies;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(NOW_PLAYING_URL, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.d(TAG,"OnSuccess");
+                JSONObject jsonObject = json.jsonObject;
+                try {
+                    JSONArray result = jsonObject.getJSONArray("results");
+                    //Log.i(TAG,"Results:" + results.toString());
+                    movies = Movie.fromJsonArray(result);
+                } catch (JSONException e)
+                { Log.i(TAG,"Movies:" + movies.size());
+                    Log.e(TAG, "Hit Json exception",e );
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.d(TAG,"OnFailure");
+            }
+        });
+    }
+}
+
+
