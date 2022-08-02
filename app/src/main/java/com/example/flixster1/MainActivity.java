7f9f112a635dc096c2846e6c8077ba8910ee3096
Clyfane <107255231+Clyfane@users.noreply.github.com>
2022-08-02 09:49:56 -0400

    correction RecyclerView

--- app/src/main/java/com/example/flixster1/MainActivity.java
+++ app/src/main/java/com/example/flixster1/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.flixster1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ListActivity;
 import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,19 +16,34 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import adapters.MovieAdapter;
 import models.Movie;
 import okhttp3.Headers;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
-    public static final String TAG = "MainActivity"; List<Movie> movies;
+    public static final String TAG = "MainActivity";
+
+    List<Movie> movies;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RecyclerView rvMovies = findViewById(R.id.rvMovies);
+        movies = new ArrayList<>();
+        // Create the adapter
+        MovieAdapter movieAdapter = new MovieAdapter( this, movies);
+
+        // Set the adapter on the recycler view
+        rvMovies.setAdapter(movieAdapter);
+
+        // Set a layout manager on the recycler view
+        rvMovies.setLayoutManager( new LinearLayoutManager( this));
 
         AsyncHttpClient client = new AsyncHttpClient();
         client.get(NOW_PLAYING_URL, new JsonHttpResponseHandler() {
@@ -34,13 +52,14 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG,"OnSuccess");
                 JSONObject jsonObject = json.jsonObject;
                 try {
-                    JSONArray result = jsonObject.getJSONArray("results");
-                    //Log.i(TAG,"Results:" + results.toString());
-                    movies = Movie.fromJsonArray(result);
-                } catch (JSONException e)
-                { Log.i(TAG,"Movies:" + movies.size());
-                    Log.e(TAG, "Hit Json exception",e );
-                    e.printStackTrace();
+                    JSONArray results = jsonObject.getJSONArray("results");
+                    Log.i(TAG,"Results:" + results.toString());
+                    movies.addAll(Movie.fromJsonArray(results));
+                    movieAdapter.notifyDataSetChanged();
+                    Log.i(TAG,"Movies:" + movies.size());
+                } catch (JSONException e) {
+                      Log.e(TAG, "Hit Json exception",e );
+
                 }
             }
 
